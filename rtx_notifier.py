c4488bbb6aff7636723c708b4395ab3b1935b7f1
john.marzulli@hotmail.com <jmarzulli@pop-os.localdomain>
2020-10-15 10:57:24 -0700

    Update products being searched for

--- rtx_notifier.py
+++ rtx_notifier.py
@@ -9,12 +9,16 @@ add_to_cart_button_search = "add to cart"
 # Update the search term for each of the vendors. the css class to look for. should be fine for a while. currenly setup for 3080
 search_details_by_vendor = {
     NEWEGG: ProductSearchDetails(
-        "https://www.newegg.com/p/pl?d=rtx+3080&N=50001315%20601357261%20100007709%204841%2050001402%2050001314&isdeptsrh=1",
+        # EVGA, MSI, ASUS, GIGABYTE
+        # **Does Not** include ZOTAC
+        "https://www.newegg.com/p/pl?d=rtx+3080&N=50001315%20601357261%20100007709%204841%2050001402%2050001314%2050001312&isdeptsrh=1",
         "item-cell",
         add_to_cart_button_search,
         2),
     BESTBUY: ProductSearchDetails(
-        "https://www.bestbuy.com/site/searchpage.jsp?_dyncharset=UTF-8&id=pcat17071&iht=y&keys=keys&ks=960&list=n&qp=brand_facet%3DBrand~ASUS%5Ebrand_facet%3DBrand~EVGA%5Ebrand_facet%3DBrand~NVIDIA&sc=Global&st=rtx%203080&type=page&usc=All%20Categories",
+        # EVGA, NVIDIA, GIGABYTE, MSI
+        # **Does Not** include PNY
+        "https://www.bestbuy.com/site/searchpage.jsp?_dyncharset=UTF-8&id=pcat17071&iht=y&keys=keys&ks=960&list=n&qp=brand_facet%3DBrand~ASUS%5Ebrand_facet%3DBrand~EVGA%5Ebrand_facet%3DBrand~GIGABYTE%5Ebrand_facet%3DBrand~MSI%5Ebrand_facet%3DBrand~NVIDIA&sc=Global&st=rtx%203080&type=page&usc=All%20Categories",
         "sku-item",
         add_to_cart_button_search,
         5)
